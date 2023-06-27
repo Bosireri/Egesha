4356fdeaec460d26e385734aa2539dd47e172998
Bosireri <bosireri24@gmail.com>
2023-06-27 10:38:03 +0300

    .myApp

---
+++ src/test/java/com/eParking/Egesha/EgeshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eParking.Egesha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgeshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
