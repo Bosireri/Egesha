6f4133c946a6335180fb73db1fba3d8740842105
Bosireri <bosireri24@gmail.com>
2023-06-27 10:38:16 +0300

    .myApp

---
+++ src/main/java/com/eParking/Egesha/EgeshaApplication.java
@@ -0,0 +1,13 @@
+package com.eParking.Egesha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgeshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgeshaApplication.class, args);
+	}
+
+}
