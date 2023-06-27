10d69ed2b5108ab1fcca511558cf75340f7aab75
Bosireri <bosireri24@gmail.com>
2023-06-27 10:37:42 +0300

    .user

---
+++ src/main/java/com/eParking/Egesha/model/LocalUser.java
@@ -0,0 +1,65 @@
+package com.eParking.Egesha.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "LocalUser")
+public class LocalUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", nullable = false)
+    private Integer userId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "username", nullable = false, unique = true, length = 20)
+    private String username;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
