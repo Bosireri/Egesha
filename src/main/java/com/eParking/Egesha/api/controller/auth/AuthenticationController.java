3297b4b0a9d175991b245535d99bbad897600ddb
Bosireri <bosireri24@gmail.com>
2023-06-29 11:51:36 +0300

    .theAuth

---
+++ src/main/java/com/eParking/Egesha/api/controller/auth/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.eParking.Egesha.api.controller.auth;
+
+import com.eParking.Egesha.api.model.RegistrationBody;
+import com.eParking.Egesha.exception.UserAlreadyExistsException;
+import com.eParking.Egesha.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    private UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity registerUser(@Valid @RequestBody RegistrationBody registrationBody) {
+        try {
+            userService.registerUser(registrationBody);
+            return ResponseEntity.ok().build();
+        } catch (UserAlreadyExistsException ex) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+}
