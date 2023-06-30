f587e5182d5c9cc3a222d8b686f6f4afb98ec179
Bosireri <bosireri24@gmail.com>
2023-06-30 08:40:30 +0300

    service.user

--- src/main/java/com/eParking/Egesha/service/UserService.java
+++ src/main/java/com/eParking/Egesha/service/UserService.java
@@ -1,20 +1,26 @@
 package com.eParking.Egesha.service;
 
+import com.eParking.Egesha.api.model.LoginBody;
 import com.eParking.Egesha.api.model.RegistrationBody;
 import com.eParking.Egesha.exception.UserAlreadyExistsException;
 import com.eParking.Egesha.model.LocalUser;
 import com.eParking.Egesha.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
+    private EncryptionService encryptionService;
+    private JWTService jwtService;
 
-    public UserService(LocalUserDAO localUserDAO) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
+        this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
-
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
         if (localUserDAO.findByEmailIgnoreCase(registrationBody.getEmail()).isPresent()
                 || localUserDAO.findByUsernameIgnoreCase(registrationBody.getUsername()).isPresent()) {
@@ -24,10 +30,19 @@ public class UserService {
         user.setEmail(registrationBody.getEmail());
         user.setFullName(registrationBody.getFullName());
         user.setUsername(registrationBody.getUsername());
-        //TODO: Encrypt passwords!!
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         user = localUserDAO.save(user);
-        return user;
+        return localUserDAO.save(user);
     }
 
+    public  String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (opUser.isPresent()) {
+            LocalUser user = opUser.get();
+            if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
+    }
 }
