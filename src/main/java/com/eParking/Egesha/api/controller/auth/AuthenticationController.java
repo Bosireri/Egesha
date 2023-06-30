820d1893b0635f856df122f6f8fc44519b42f763
Bosireri <bosireri24@gmail.com>
2023-06-30 08:41:38 +0300

    theAuth

--- src/main/java/com/eParking/Egesha/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/eParking/Egesha/api/controller/auth/AuthenticationController.java
@@ -1,11 +1,12 @@
 package com.eParking.Egesha.api.controller.auth;
 
+import com.eParking.Egesha.api.model.LoginBody;
+import com.eParking.Egesha.api.model.LoginResponse;
 import com.eParking.Egesha.api.model.RegistrationBody;
 import com.eParking.Egesha.exception.UserAlreadyExistsException;
 import com.eParking.Egesha.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +27,18 @@ public class AuthenticationController {
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExistsException ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginBody loginBody) {
+        String jwt = userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            LoginResponse response = new LoginResponse();
+            response.setJwt(jwt);
+            return ResponseEntity.ok(response);
         }
     }
 
