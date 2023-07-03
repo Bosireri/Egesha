560f2efd755e89cc22e80eaf3a8bd848c18a79ad
Bosireri <bosireri24@gmail.com>
2023-07-03 16:50:46 +0300

    theAuth

--- src/main/java/com/eParking/Egesha/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/eParking/Egesha/api/controller/auth/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/userAuth")
 public class AuthenticationController {
 
     private UserService userService;
