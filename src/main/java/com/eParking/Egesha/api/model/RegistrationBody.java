c9c39b09921a0aa80a8bb3a106724ea489b6361b
Bosireri <bosireri24@gmail.com>
2023-07-05 19:54:37 +0300

    userRegistration

--- src/main/java/com/eParking/Egesha/api/model/RegistrationBody.java
+++ src/main/java/com/eParking/Egesha/api/model/RegistrationBody.java
@@ -12,10 +12,10 @@ public class RegistrationBody {
     @NotBlank
     private String lastName;
 
-    @NotNull
-    @NotBlank
-    @Size(min = 10, max = 13)
-    private Integer phoneNumber;
+    @NotNull(message = "Phone Number is required")
+    @Min(value = 1000000000L, message = "Phone Number must have at least 10 digits")
+    @Max(value = 9999999999999L, message = "Phone Number must have at most 13 digits")
+    private Long phoneNumber;
 
     @NotNull
     @NotBlank
@@ -36,7 +36,7 @@ public class RegistrationBody {
         return lastName;
     }
 
-    public Integer getPhoneNumber() {
+    public Long getPhoneNumber() {
         return phoneNumber;
     }
 
