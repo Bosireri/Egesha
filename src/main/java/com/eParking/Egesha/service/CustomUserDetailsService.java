80c67ff4a4d276ed975a0f23086366dac4b39a30
Bosireri <bosireri24@gmail.com>
2023-07-05 20:03:11 +0300

    service.UserDetails

---
+++ src/main/java/com/eParking/Egesha/service/CustomUserDetailsService.java
@@ -0,0 +1,62 @@
+package com.eParking.Egesha.service;
+
+import com.eParking.Egesha.model.Admin;
+import com.eParking.Egesha.model.LocalUser;
+import com.eParking.Egesha.model.UserType;
+import com.eParking.Egesha.model.dao.AdminRepository;
+import com.eParking.Egesha.model.dao.LocalUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private LocalUserRepository localUserRepository;
+
+    private UserType userType;
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println(userType);
+
+        if(userType == UserType.ADMIN) {
+            Admin admin = adminRepository.findByUsername(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("Admin Username" + username + "not found"));
+            SimpleGrantedAuthority adminAuthority = new SimpleGrantedAuthority(UserType.ADMIN.toString());
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(adminAuthority);
+            return new User(admin.getUsername(), admin.getPassword(), authorities);
+
+        } else if (userType == UserType.LOCALUSER) {
+            LocalUser localUser = localUserRepository.findByEmailIgnoreCase(username)
+                    .orElseThrow(() -> new UsernameNotFoundException("LocalUser Email "+ username + "not found"));
+            SimpleGrantedAuthority adminAuthority = new SimpleGrantedAuthority(UserType.LOCALUSER.toString());
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(adminAuthority);
+            return new User(localUser.getEmail(), localUser.getPassword(), authorities);
+        }
+        return null;
+    }
+
+}
