5fd96c778d6c00b3290e99a43812e50e276cd4d8
Bosireri <bosireri24@gmail.com>
2023-07-05 19:59:56 +0300

    generator()

---
+++ src/main/java/com/eParking/Egesha/api/security/JWTGenerator.java
@@ -0,0 +1,45 @@
+package com.eParking.Egesha.api.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTGenerator {
+    public String generateToken(Authentication authentication, String userType) {
+        String username = authentication.getName();
+        Date currentDate = new Date();
+        Date expiryDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+        String token = Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(currentDate)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, SecurityConstants.JWT_SECRET)
+                .claim("userType", userType)
+                .compact();
+        return token;
+    }
+
+    public String getUsernameFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(SecurityConstants.JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("userType").toString();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SecurityConstants.JWT_SECRET).parseClaimsJws(token);
+            return true;
+        }
+        catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT token not Valid" + token);
+        }
+    }
+}
