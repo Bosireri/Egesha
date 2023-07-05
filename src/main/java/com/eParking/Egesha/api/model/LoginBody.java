00ddee096ab8a1cc331200f6590a7f8ffaa87666
Bosireri <bosireri24@gmail.com>
2023-07-05 19:53:49 +0300

    userLogin

--- src/main/java/com/eParking/Egesha/api/model/LoginBody.java
+++ src/main/java/com/eParking/Egesha/api/model/LoginBody.java
@@ -1,19 +1,22 @@
 package com.eParking.Egesha.api.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public class LoginBody {
 
-    @NotNull
-    @NotBlank
-    private Integer phoneNumber;
+    @NotNull(message = "Phone Number is required")
+    @Min(value = 1000000000L, message = "Phone Number must have at least 10 digits")
+    @Max(value = 9999999999999L, message = "Phone Number must have at most 13 digits")
+    private Long phoneNumber;
 
     @NotNull
     @NotBlank
     private String password;
 
-    public Integer getPhoneNumber() {
+    public Long getPhoneNumber() {
         return phoneNumber;
     }
 
