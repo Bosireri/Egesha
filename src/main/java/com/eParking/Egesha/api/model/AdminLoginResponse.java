86344b5b0443c6844159d6e679d89222ad916ae8
Bosireri <bosireri24@gmail.com>
2023-07-05 19:57:34 +0300

    adminResponse

---
+++ src/main/java/com/eParking/Egesha/api/model/AdminLoginResponse.java
@@ -0,0 +1,45 @@
+package com.eParking.Egesha.api.model;
+
+    
+public class AdminLoginResponse {
+
+    private String jwt;
+    private String token;
+    private boolean success;
+    private String message;
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setAdmin(String username, Integer id) {
+    }
+}
