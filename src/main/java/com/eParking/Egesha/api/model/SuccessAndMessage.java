cd3a3a730c13f4c7f885e648ba03fe48612e292e
Bosireri <bosireri24@gmail.com>
2023-07-05 20:01:10 +0300

    se

---
+++ src/main/java/com/eParking/Egesha/api/model/SuccessAndMessage.java
@@ -0,0 +1,23 @@
+package com.eParking.Egesha.api.model;
+
+public class SuccessAndMessage {
+
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
