c09110a745cdc3dfe804db0a698d63e8330feca1
Bosireri <bosireri24@gmail.com>
2023-07-05 19:56:49 +0300

    reAdmin

---
+++ src/main/java/com/eParking/Egesha/api/model/AdminLoginBody.java
@@ -0,0 +1,23 @@
+package com.eParking.Egesha.api.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class AdminLoginBody {
+
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @NotNull
+    @NotBlank
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
