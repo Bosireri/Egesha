457862cfbc1a9e621a30e1998322b50894bd73c4
Bosireri <bosireri24@gmail.com>
2023-07-05 19:56:21 +0300

    info.Admin

---
+++ src/main/java/com/eParking/Egesha/model/Admin.java
@@ -0,0 +1,42 @@
+package com.eParking.Egesha.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin_details")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
