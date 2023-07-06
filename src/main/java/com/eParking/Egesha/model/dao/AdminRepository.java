f9abd35acf9608446d1835b349f1abb11006c092
Bosireri <bosireri24@gmail.com>
2023-07-06 22:14:17 +0300

    adminDataAccessObject

--- src/main/java/com/eParking/Egesha/model/dao/AdminRepository.java
+++ src/main/java/com/eParking/Egesha/model/dao/AdminRepository.java
@@ -1,11 +1,14 @@
 package com.eParking.Egesha.model.dao;
 
 import com.eParking.Egesha.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface AdminRepository extends CrudRepository<Admin, Long> {
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
 
     Optional<Admin> findByUsername(String username);
     Boolean existsByUsername(String username);
