7dd050500279da19a0dd8a5fe2ecca38351ada62
Bosireri <bosireri24@gmail.com>
2023-07-06 22:22:08 +0300

    securityConfigurer

--- src/main/java/com/eParking/Egesha/api/security/WebSecurityConfig.java
+++ src/main/java/com/eParking/Egesha/api/security/WebSecurityConfig.java
@@ -3,17 +3,59 @@ package com.eParking.Egesha.api.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import com.eParking.Egesha.model.UserType;
 @Configuration
 public class WebSecurityConfig {
+
+    private CustomUserDetailsService customUserDetailsService;
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
 
     @Autowired
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable().cors().disable();
-        http.authorizeHttpRequests().anyRequest().permitAll();
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .requestMatchers("/auth/registerAdmin").permitAll()
+                .requestMatchers("/auth/loginAdmin").permitAll()
+                .requestMatchers("/auth/registerUser").permitAll()
+                .requestMatchers("/auth/loginUser").permitAll()
+                .requestMatchers("/auth/updateUser").permitAll()
+                .requestMatchers("/admin/auth/**").hasAuthority(UserType.ADMIN.toString())
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
+    }
+    
 }
