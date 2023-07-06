b7559bfeaaad0f6756920c7092ac897431631e57
Bosireri <bosireri24@gmail.com>
2023-07-06 22:24:12 +0300

    ...

--- src/main/java/com/eParking/Egesha/api/security/SecurityConstants.java
+++ src/main/java/com/eParking/Egesha/api/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.eParking.Egesha.api.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 18000;
-    public static final String JWT_SECRET = "pokemonsecretrandomstringwithmorethan256bits";
+    public static final long JWT_EXPIRATION = 180000000;
+    public static final String JWT_SECRET = "2B7E151628AED2A6ABF7158809CF4F3CB480013RI3ECLECTICS";
 }
