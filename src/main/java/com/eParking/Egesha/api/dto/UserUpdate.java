c6c56d5e574a75cf2f88a6b0289e88143449fdbe
Bosireri <bosireri24@gmail.com>
2023-07-06 22:25:13 +0300

    updateUserInfo

---
+++ src/main/java/com/eParking/Egesha/api/dto/UserUpdate.java
@@ -0,0 +1,50 @@
+package com.eParking.Egesha.api.dto;
+
+public class UserUpdate {
+
+    private String firstName;
+    private String lastName;
+    private Long phoneNumber;
+    private String email;
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
