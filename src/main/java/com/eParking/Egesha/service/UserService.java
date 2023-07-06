ac1121324cef40f2dfba5fad7a68b6079f4ca53b
Bosireri <bosireri24@gmail.com>
2023-07-06 22:06:38 +0300

    service.User

--- src/main/java/com/eParking/Egesha/service/UserService.java
+++ src/main/java/com/eParking/Egesha/service/UserService.java
@@ -1,10 +1,15 @@
 package com.eParking.Egesha.service;
 
-import com.eParking.Egesha.api.model.LoginBody;
-import com.eParking.Egesha.api.model.RegistrationBody;
+import com.eParking.Egesha.api.dto.LoginBody;
+import com.eParking.Egesha.api.dto.RegistrationBody;
+import com.eParking.Egesha.api.dto.SuccessAndMessage;
+import com.eParking.Egesha.api.dto.UserUpdate;
 import com.eParking.Egesha.exception.UserAlreadyExistsException;
 import com.eParking.Egesha.model.LocalUser;
 import com.eParking.Egesha.model.dao.LocalUserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +20,7 @@ public class UserService {
     private LocalUserRepository localUserRepository;
     private EncryptionService encryptionService;
     private JWTService jwtService;
+    private PasswordEncoder passwordEncoder;
 
     public UserService(LocalUserRepository localUserRepository, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserRepository = localUserRepository;
@@ -47,4 +53,26 @@ public class UserService {
         }
         return null;
     }
+
+    public ResponseEntity<SuccessAndMessage> updateUser(Integer userId, UserUpdate userUpdate){
+        System.out.println("userUpdate");
+        SuccessAndMessage response = new SuccessAndMessage();
+        if (!(localUserRepository.existsById(userId))) {
+            response.setMessage("User does not exist");
+            response.setSuccess(false);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+        Optional<LocalUser> user = localUserRepository.findById(userId);
+        LocalUser localUser = user.get();
+        localUser.setFirstName(userUpdate.getFirstName());
+        localUser.setLastName(userUpdate.getLastName());
+        localUser.setPhoneNumber(userUpdate.getPhoneNumber());
+        localUser.setEmail(userUpdate.getEmail());
+        localUser.setPassword(passwordEncoder.encode(userUpdate.getPassword()));
+        localUserRepository.save(localUser);
+        response.setMessage("User Updated Successfully");
+        response.setSuccess(true);
+        return new ResponseEntity<SuccessAndMessage>(response, HttpStatus.OK);
+    }
+
 }
