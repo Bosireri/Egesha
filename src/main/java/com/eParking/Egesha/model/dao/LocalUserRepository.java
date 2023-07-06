ba0be0dabc8ced46e273deb7650e8561f1baa833
Bosireri <bosireri24@gmail.com>
2023-07-06 22:02:48 +0300

    userRepo

--- src/main/java/com/eParking/Egesha/model/dao/LocalUserRepository.java
+++ src/main/java/com/eParking/Egesha/model/dao/LocalUserRepository.java
@@ -2,11 +2,13 @@ package com.eParking.Egesha.model.dao;
 
 
 import com.eParking.Egesha.model.LocalUser;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
-public interface LocalUserRepository extends CrudRepository<LocalUser, Long> {
+@Repository
+public interface LocalUserRepository extends JpaRepository<LocalUser, Integer> {
 
     Optional<LocalUser> findByPhoneNumber(Long phoneNumber);
     Optional<LocalUser> findByEmailIgnoreCase(String email);
