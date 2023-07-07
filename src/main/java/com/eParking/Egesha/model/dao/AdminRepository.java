37050859cd38cd678705cf09d47b4e844cfb9823
Bosireri <bosireri24@gmail.com>
2023-07-07 22:10:14 +0300

    theRepo.admin

--- src/main/java/com/eParking/Egesha/model/dao/AdminRepository.java
+++ src/main/java/com/eParking/Egesha/model/dao/AdminRepository.java
@@ -2,7 +2,6 @@ package com.eParking.Egesha.model.dao;
 
 import com.eParking.Egesha.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
