80b8687202c2f6166de5c61360ac8f9aa2fcd2c8
Bosireri <bosireri24@gmail.com>
2023-07-07 21:15:42 +0300

    userProfileEntity

---
+++ src/main/java/com/eParking/Egesha/model/UserProfile.java
@@ -0,0 +1,46 @@
+package com.eParking.Egesha.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
