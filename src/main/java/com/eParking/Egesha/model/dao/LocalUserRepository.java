7bbfd796a21143d0cc9a33fd2c02c9bde2041d41
Bosireri <bosireri24@gmail.com>
2023-07-07 22:09:41 +0300

    theRepo

--- src/main/java/com/eParking/Egesha/model/dao/LocalUserRepository.java
+++ src/main/java/com/eParking/Egesha/model/dao/LocalUserRepository.java
@@ -3,7 +3,6 @@ package com.eParking.Egesha.model.dao;
 
 import com.eParking.Egesha.model.LocalUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
