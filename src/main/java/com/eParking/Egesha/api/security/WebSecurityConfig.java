dc05b77c587931315d1a7621e1ba02e3fc21a0b0
Bosireri <bosireri24@gmail.com>
2023-07-12 19:01:39 +0300

    securityConfig

--- src/main/java/com/eParking/Egesha/api/security/WebSecurityConfig.java
+++ src/main/java/com/eParking/Egesha/api/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,14 +15,16 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import com.eParking.Egesha.model.UserType;
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
+    @Autowired
     private CustomUserDetailsService customUserDetailsService;
+    @Autowired
     private JwtAuthEntryPoint jwtAuthEntryPoint;
 
-    @Autowired
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
                 .exceptionHandling()
