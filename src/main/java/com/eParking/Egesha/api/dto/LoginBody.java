1e68dc2185fe6a0dd8f6f6f4e18db4b38e761f6e
Bosireri <bosireri24@gmail.com>
2023-07-12 19:04:59 +0300

    userLoginBody

--- src/main/java/com/eParking/Egesha/api/dto/LoginBody.java
+++ src/main/java/com/eParking/Egesha/api/dto/LoginBody.java
@@ -1,28 +1,27 @@
 package com.eParking.Egesha.api.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public class LoginBody {
 
-    @NotNull(message = "Phone Number is required")
-    @Min(value = 1000000000L, message = "Phone Number must have at least 10 digits")
-    @Max(value = 9999999999999L, message = "Phone Number must have at most 13 digits")
-    private Long phoneNumber;
+//    @NotNull(message = "Phone Number is required")
+//    @Min(value = 1000000000L, message = "Phone Number must have at least 10 digits")
+//    @Max(value = 9999999999999L, message = "Phone Number must have at most 13 digits")
+//    private Long phoneNumber;
+    @Email
+    private String email;
 
     @NotNull
     @NotBlank
     private String password;
 
-    public Long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+//    public Long getPhoneNumber() {
+//        return phoneNumber;
+//    }
+//
+//    public void setPhoneNumber(Long phoneNumber) {
+//        this.phoneNumber = phoneNumber;
+//    }
 
     public String getPassword() {
         return password;
@@ -30,5 +29,13 @@ public class LoginBody {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
