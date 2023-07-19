9d15516867508c2d56960d9ef51eac1d49b4400c
Bosireri <122856808+Bosireri@users.noreply.github.com>
2023-07-19 23:56:09 +0300

    Update UserProfileController.java

--- src/main/java/com/eParking/Egesha/api/controller/auth/UserProfileController.java
+++ src/main/java/com/eParking/Egesha/api/controller/auth/UserProfileController.java
@@ -12,9 +12,9 @@
 //import org.springframework.web.bind.annotation.RequestPart;
 //import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.multipart.MultipartFile;
-//
+
 //import java.io.IOException;
-//
+
 //@RestController
 //@RequestMapping("/auth")
 //public class UserProfileController {
@@ -23,7 +23,7 @@
 //    private UserProfileService userProfileService;
 //    @Autowired
 //    private UserProfileRepository userProfileRepository;
-//
+
 //    //should this emdpoint be a POST or PUT
 //    //I think it should be a and update user endpoint
 //    @PostMapping("/profile")
